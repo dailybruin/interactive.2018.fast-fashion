4eb642bb772ecbc6dc2fc7b1a3121845d486be91
Michael <myque@live.com>
2018-05-23 12:30:27 -0700

    finalized formatting and data

--- src/article.js
+++ src/article.js
@@ -1,12 +1,12 @@
 let button = document.getElementById("return-button");
 button.addEventListener("mouseenter", () => {
   console.log("enter");
-  button.style.backgroundColor = "rgb(226, 170, 106)";
-  button.innerHTML = "SPRING SING 2018"
+  button.style.backgroundColor = "rgb(207, 207, 207)";
+  button.innerHTML = "FAST 2018"
 });
 
 button.addEventListener("mouseleave", () => {
   console.log("exit");
-  button.style.backgroundColor = "#D19E64";
+  button.style.backgroundColor = "white";
   button.innerHTML = "RETURN HOME"
 });
